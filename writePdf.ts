add01d9f49256aeb170ec19bb1409a8f7414bce4
e-krebs <e-krebs@users.noreply.github.com>
2021-01-24 23:00:49 +0100

    update writePdf: deal with MMS

--- writePdf.ts
+++ writePdf.ts
@@ -5,69 +5,110 @@ dotenv.config();
 import PDFDocument from 'pdfkit';
 import fs from 'fs';
 
-import { Message, PdfConfig, SMSConfig } from './typings';
-import { me, other, addMessage, computeNewDay, showHour, finishSms, shortenEmojis } from './utils';
-import { page, font, textWidth } from './pdfConfig';
+import { ImageInfo, Message, MMSConfig, PdfConfig, SMSConfig } from './typings';
+import { me, other, addMessage, computeNewDay, showHour, finishSms, computeHeight, computeMmsHeight, writeImage } from './utils';
+import { page, font } from './pdfConfig';
 
-const messages: Message[] = JSON.parse(fs.readFileSync('sms-clean.json').toString());
+const main = async () => {
+  const messages: Message[] = JSON.parse(fs.readFileSync('sms-clean.json').toString());
 
-const config: PdfConfig = {
-  doc: new PDFDocument({
-    size: [page.width, page.height],
-    bufferPages: true
-  }),
-  errors: '',
-  currentDate: '',
-  currentTime: 0,
-  currentSource: '',
-  nextY: page.margin
-};
+  const config: PdfConfig = {
+    doc: new PDFDocument({
+      size: [page.width, page.height],
+      bufferPages: true
+    }),
+    errors: '',
+    currentDate: '',
+    currentTime: 0,
+    currentSource: '',
+    nextY: page.margin
+  };
 
-config.doc.pipe(fs.createWriteStream('sms.pdf') as NodeJS.WritableStream);
-config.doc.font('fonts/NotoSans-ExtraLight.ttf');
+  config.doc.pipe(fs.createWriteStream('sms.pdf') as NodeJS.WritableStream);
+  config.doc.font('fonts/NotoSans-ExtraLight.ttf');
 
-config.doc.fontSize(font.title);
-config.doc.text(`${other} & ${me}`, { align: 'center' });
-config.doc.addPage();
+  // cover page
+  config.doc.fontSize(font.title);
+  config.doc.text(`${other} & ${me}`, { align: 'center' });
+  config.doc.addPage();
 
-// add an empty page
-config.doc.addPage();
+  // add an empty page
+  config.doc.addPage();
 
-// reset
-config.doc.fontSize(font.regular);
+  // reset
+  config.doc.fontSize(font.regular);
 
-for (const message of messages) {
-  switch (message.type) {
-    case 'SMS':
-      const msgConfig: SMSConfig = {
-        message,
-        align: message.source === me ? 'right' : 'left',
-        height: config.doc.heightOfString(
-          shortenEmojis(message.message),
-          { width: textWidth }
-        ),
-        showHour: false
-      };
-
-      computeNewDay(msgConfig, config);
-
-      if (msgConfig.showHour) {
-        showHour(msgConfig, config)
-      }
-
-      addMessage(msgConfig, config);
-
-      finishSms(msgConfig, config);
-      break;
-    case 'MMS':
-      console.log('TODO: mms');
-      break;
-    default:
-      console.log('malformed message', message);
-      break;
+  if (fs.existsSync('tmp')) {
+    fs.rmSync('tmp', { recursive: true });
   }
+  fs.mkdirSync('tmp');
+
+  for (const message of messages) {
+    switch (message.type) {
+      case 'SMS':
+        const msgConfig: SMSConfig = {
+          message,
+          align: message.source === me ? 'right' : 'left',
+          height: computeHeight(message.message, config.doc),
+          showHour: false
+        };
+
+        computeNewDay(msgConfig, config);
+
+        if (msgConfig.showHour) {
+          showHour(msgConfig, config)
+        }
+
+        addMessage(msgConfig, config, null);
+
+        finishSms(msgConfig, config);
+        break;
+      case 'MMS':
+        let imageInfo: ImageInfo | null = null;
+        if (message.message.gif) {
+          imageInfo = await writeImage(message.message.gif, 'gif');
+        } else if (message.message.jpeg) {
+          imageInfo = await writeImage(message.message.jpeg, 'jpeg');
+        } else if (message.message.png) {
+          imageInfo = await writeImage(message.message.png, 'png');
+        } else if (message.message.audio) {
+          console.log('\nTODO: MMS audio');
+        }
+
+        const mmsConfig: MMSConfig = {
+          message,
+          align: message.source === me ? 'right' : 'left',
+          height: computeMmsHeight(message.message, imageInfo, config.doc),
+          showHour: false
+        };
+
+        computeNewDay(mmsConfig, config);
+
+        if (mmsConfig.showHour) {
+          showHour(mmsConfig, config)
+        }
+
+        addMessage(mmsConfig, config, imageInfo);
+
+        finishSms(mmsConfig, config);
+
+        break;
+      default:
+        console.log('malformed message', message);
+        break;
+    }
+  }
+
+  config.doc.end();
+
+  
+  if (fs.existsSync('tmp')) {
+    fs.rmSync('tmp', { recursive: true });
+  }
+
+  fs.writeFileSync('errors.log', config.errors);
 }
 
-config.doc.end();
-
-fs.writeFileSync('errors.log', config.errors);
+main().then(() => {
+  console.log('sms.pdf file is written');
+})
