dd68e825fb2c10c254d82883a8c8473c65f57d31
e-krebs <e-krebs@users.noreply.github.com>
2021-02-14 19:56:34 +0100

    update circleImage

--- utils/circleImage.ts
+++ utils/circleImage.ts
@@ -4,33 +4,47 @@ import { ImageInfo } from '../typings';
 import { gm } from './gm';
 import { resizeImage } from './mmsHelper';
 
-export const circleImage = async (inputImageInfo: ImageInfo): Promise<ImageInfo | null> => {
-  const imageInfo: ImageInfo = {
-    path: `tmp/${inputImageInfo.path}`,
-    width: Math.round(inputImageInfo.width),
-    height: Math.round(inputImageInfo.height)
-  };
-  const halfSize = Math.round(inputImageInfo.width / 2);
+const wait = async (): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(() => { resolve(); }, 1000);
+  });
+}
 
+export const circleImage = async (inputImageInfo: ImageInfo): Promise<ImageInfo | null> => {
+  let size: number = Math.round(inputImageInfo.width);
+  if (size % 2 === 0) {
+    size += 1;
+  }
+  let imageInfo: ImageInfo = {
+    path: `tmp/${inputImageInfo.path}`,
+    width: size,
+    height: size
+  };
+  let finalImageInfo: ImageInfo = {
+    path: `tmp/circle_${inputImageInfo.path}`,
+    width: size,
+    height: size
+  };
+  const halfSize = Math.round(size / 2);
   fs.copyFileSync(inputImageInfo.path, imageInfo.path);
 
-
   await resizeImage(imageInfo);
+  await wait();
 
   /*
   convert -size 285x285 xc:none -fill .\other.jpg -draw "circle 143,143 143,1" .\other_circle.png
   */
   return new Promise((resolve) => {
-    gm(imageInfo.width, imageInfo.height).command('convert')
+    gm(size, size).command('convert')
       .in('xc:none')
       .in('-fill', imageInfo.path)
       .in('-draw', `circle ${halfSize},${halfSize} ${halfSize},1`)
-      .write(imageInfo.path, (err) => {
+      .write(finalImageInfo.path, (err) => {
         if (err) {
-          console.log(`\n🛑 ${imageInfo.path} error when circling image`, err);
+          console.log(`\n🛑 ${finalImageInfo.path} error when circling image`, err);
           resolve(null);
         }
-        resolve(imageInfo);
+        resolve(finalImageInfo);
       });
   });
 };
